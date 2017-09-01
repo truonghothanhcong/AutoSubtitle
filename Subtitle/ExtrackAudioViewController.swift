72d262c7aa84cf68d5b56728a1280ab8b2748774
CongTruong <cong@Macbooks-MacBook-Pro.local>
2017-09-01 13:21:25 +0700

    [Add] project done recognize speech + translate with gg api

---
+++ Subtitle/ExtrackAudioViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ExtrackAudioViewController.swift
+//  Subtitle
+//
+//  Created by CongTruong on 8/6/17.
+//  Copyright © 2017 congtruong. All rights reserved.
+//
+
+import UIKit
+
+class ExtrackAudioViewController: UIViewController {
+
+    @IBOutlet weak var myView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+}
+
+
---
+++ Subtitle/ViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ViewController.swift
+//  Subtitle
+//
+//  Created by CongTruong on 8/6/17.
+//  Copyright © 2017 congtruong. All rights reserved.
+//
+
+import UIKit
+import Speech
+import ROGoogleTranslate
+
+@available(iOS 10.0, *)
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+
+    @IBOutlet weak var subtitleTextView: UITextView!
+    @IBOutlet weak var microphoneButton: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))  //1
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    var goolgeTranslate : ROGoogleTranslate!
+    var params : ROGoogleTranslateParams!
+    
+    var threadRefreshLabel: Thread!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        microphoneButton.isEnabled = false  //2
+        
+        speechRecognizer?.delegate = self  //3
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.goolgeTranslate = ROGoogleTranslate()
+        goolgeTranslate.apiKey = "AIzaSyCwkDfP2_Bl7B6w_N5l-j-yY6D5slc0PLw"
+        
+        params = ROGoogleTranslateParams()
+        params.source = "en"
+        params.target = "vi"
+    }
+    
+    @IBAction func microphoneTapped(_ sender: UIButton) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    
+    // ==================================================== //
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                self.params.text = result?.bestTranscription.formattedString ?? ""
+                
+                self.goolgeTranslate.translate(params: self.params, callback: { (result) in
+                    
+                    DispatchQueue.main.async {
+                        self.subtitleTextView.text = result
+                    }
+                })
+                
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        subtitleTextView.text = "Say something, I'm listening!"
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
+    }
+}
+
